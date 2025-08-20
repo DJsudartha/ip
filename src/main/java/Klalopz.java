d40e054c37293c0a8d4a778882c9e5a5eca448d1
DJsudartha <156521721+DJsudartha@users.noreply.github.com>
2025-08-20 12:34:24 +0800

    Added scanner for inputs

--- src/main/java/Klalopz.java
+++ src/main/java/Klalopz.java
@@ -1,14 +1,33 @@
+import java.util.Scanner;
+
 public class Klalopz {
     public static void main(String[] args) {
         String botName = "Klalopz";
         String lineGap = "____________________________________________________________";
         String introMessage = "Hello! I'm " + botName + "!\nWhat can I do for you today?";
         String closingMessage = "Bye-bye, hope to see you soon!";
+        Scanner scanner = new Scanner(System.in);
+        String currInput;
 
         System.out.println(lineGap);
         System.out.println(introMessage);
         System.out.println(lineGap);
+
+        while(true) {
+            System.out.println("Your input: ");
+            currInput = scanner.nextLine().trim();
+            System.out.println(lineGap);
+
+            if (currInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(currInput);
+            System.out.println(lineGap);
+
+
+        }
         System.out.println(closingMessage);
+        System.out.println(lineGap);
 
 
 
