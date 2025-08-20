006f5e30f825e30e6299b18c7fec25ba76de04b8
DJsudartha <156521721+DJsudartha@users.noreply.github.com>
2025-08-20 10:25:50 +0800

    Changed chatbot's name and added default text

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Klalopz User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Klalopz.java
@@ -0,0 +1,16 @@
+public class Klalopz {
+    public static void main(String[] args) {
+        String botName = "Klalopz";
+        String lineGap = "____________________________________________________________";
+        String introMessage = "Hello! I'm " + botName + "!\nWhat can I do for you today?";
+        String closingMessage = "Bye-bye, hope to see you soon!";
+
+        System.out.println(lineGap);
+        System.out.println(introMessage);
+        System.out.println(lineGap);
+        System.out.println(closingMessage);
+
+
+
+    }
+}
