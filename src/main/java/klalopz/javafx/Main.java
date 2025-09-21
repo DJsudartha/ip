72546eca405deea346ab02d971694cd73fe56c79
DJsudartha <156521721+DJsudartha@users.noreply.github.com>
2025-09-21 17:56:59 +0800

    javafx auto resizing

--- src/main/java/klalopz/javafx/Main.java
+++ src/main/java/klalopz/javafx/Main.java
@@ -32,9 +32,13 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+
             Scene scene = new Scene(ap);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             stage.setScene(scene);
             stage.setTitle("Klalopz");
+
             fxmlLoader.<MainWindow>getController().setKlalopz(klalopz);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
